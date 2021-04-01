2065a578faa624cc2f4dc83f207ff432e737ed74
MusicDev33 <smccowan33@gmail.com>
2021-04-01 10:13:42 -0600

    Init with container

---
+++ src/PContainer.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Home from './pages/Home/Home';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useLocation,
+  withRouter
+} from 'react-router-dom';
+
+import env from 'react-dotenv';
+
+export class HContainer extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tissueResults: [],
+      baseUrlLen: env.BASE_URL.split('/').length
+    }
+
+    this.getTissueData = this.getTissueData.bind(this);
+    console.log(`Env: ${env.BASE_URL}`);
+  }
+
+  getTissueData(data) {
+    console.log(data);
+    window.history.replaceState(null, 'Test', '/tissue');
+  }
+
+  render() {
+    return (
+      <Router>
+        <Container fluid className="App px-0">
+          <Switch>
+            <Route path={`${env.BASE_URL}/home`}>
+              <Home sendTissueData={this.getTissueData} />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>);
+  }
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
