e2bd12ac6567096e3050de8ff661fffb1c394cf2
MusicDev33 <smccowan33@gmail.com>
2021-12-03 14:39:54 -0700

    added mongodb

--- src/pages/Docs/docfiles/databases.js
+++ src/pages/Docs/docfiles/databases.js
@@ -7,6 +7,14 @@ const databases = () => {
         these over and over again uses bandwidth and eats up storage space. This is why we offer databases from a central location. This location 
         is in the <code>/home/shared_dbs</code> folder. Any bioinformatics databases we want to offer centrally are available here. 
       </p>
+
+      <h4>MongoDB</h4>
+
+      <p>
+        The way we use MongoDB on the cluster is very different than the way the other databases are handled. The other databases all have the necessary
+        data inside already. MongoDB is what we use to collect data from various software across the HPC, from letting people create appointments
+        and contact us to tracking uptime and various system metrics. <b>This needs to stay running at all costs!</b>
+      </p>
     </div>
   );
 }
