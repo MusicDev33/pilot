a47d7fd9ba2b5192082f2ba3f347e80f2864c9b4
MusicDev33 <smccowan33@gmail.com>
2021-12-04 13:16:01 -0700

    backups

--- src/pages/Docs/docfiles/backups.js
+++ src/pages/Docs/docfiles/backups.js
@@ -5,11 +5,11 @@ const backups = () => {
       <p>
         Backups are the backbone of our entire operation. Well, there are a few backbones. We're a little better than your average vertebrate. But the backbone we'll
         be talking about here is backups. Basically if they're not happening, then that's bad. Backups should always be happening. And automated, but we'll get to that in a bit.
-        Just know that this is a very important task, and it should be a high priority at all times. HPcs have many moving parts, and things break. Whatever, no biggie.
+        Just know that this is a very important task, and it should be a high priority at all times. HPCs have many moving parts, and things break. Whatever, no biggie.
         Unless you don't have backups. That's a real problem. Here's how we deal with that.
       </p>
 
-      <h2 className="mb-4">Rsync</h2>
+      <h4>Rsync</h4>
       <p>
         There are two places where we store backups: NAS1 and NAS2. NAS1 holds long term backups, and NAS2 holds weekly backups. They're stored in different formats
         too. NAS1 holds backups in <code>.tar.gz</code> format, while NAS2 just holds straight copies of the data. If someone's missing data, you can go find it in
@@ -18,11 +18,19 @@ const backups = () => {
         a ton of useless data, like various caches and configs. Rsync backups happen every Sunday at 1 AM. 
       </p>
 
-      <h2 className="mb-4">Archives</h2>
+      <h4>Archives</h4>
       <p>
         Archives are stored on NAS1. These are monthly backups, and are currently not completely automated. These archives take a while to create, so it's a good
         idea to detach the processes and output the results into a text file. These are stored in the format <code>yyyy-mm-dd.username.tar.gz</code>.
         These are all in the <code>weekly</code> folder in NAS1.
+      </p>
+
+      <h4>Why Both?</h4>
+      <p>
+        Using Rsync creates a copy of the data that isn't compressed. This uses up <i>way</i> more space, so you can't keep as many of these backups around.
+        On the flip side, data recovery is much faster (instant) because the data just needs to be copied instead of unarchived. With the archives, we can 
+        keep more of them, so data recovery can go further back in time, but because we typically archive terabytes of data, it can be up to a day or two before 
+        we can recover the data. Using both methods gives us some flexibility in our data recovery depending on the situation.
       </p>
     </div>
   );
